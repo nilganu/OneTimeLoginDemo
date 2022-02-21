df8445eca4d4c1324d73bc3788f1139ea9129bb3
Admin <nilayganatra1987@gmail.com>
2022-02-21 10:30:54 +0530

    Comment added

--- app/src/main/java/com/cahrusat/onetimelogindiv2/MainActivity.kt
+++ app/src/main/java/com/cahrusat/onetimelogindiv2/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    //Simple Project
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
